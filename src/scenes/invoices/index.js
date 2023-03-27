0d337a97adac04c86c8dad008d93a84ee8aa431c
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-03-27 17:38:28 +0530

    Completed Invoices component too.

---
+++ src/scenes/invoices/index.js
@@ -0,0 +1,85 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
+
+const Invoices = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  //   It defines the columns for the data grid in the columns array, including the fields "id", "name", "phone", "email", "cost", and "date". It also sets the flex property to control the width of each column and uses the renderCell property to customize the rendering of the "cost" column, displaying the cost with a green color using the Typography component.
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
+
+  //   The function then returns a Box component containing a Header component with the title "INVOICES" and subtitle "List of Invoice Balances". It also contains a DataGrid component with the checkboxSelection property set to enable checkbox selection and with the rows and columns properties set to the mockDataInvoices object and the columns array respectively. Finally, it styles the data grid using the sx property to customize various elements such as the grid cells, column headers, checkbox, and footer.
+  return (
+    <Box m="20px">
+      <Header title="INVOICES" subtitle="List of Invoice Balances" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Invoices;
